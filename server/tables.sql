466d5d65670605ee07be71538e15bf2cd1362a8c
Hariharan2907 <56897205+Hariharan2907@users.noreply.github.com>
2021-11-23 15:44:55 -0600

    Initial upload of MySQL table creation

---
+++ server/tables.sql
@@ -0,0 +1,45 @@
+-- Install MySQL and manually create a SCHEMA called gomarvelfacts
+-- Run the queries below to create all the tables 
+
+USE gomarvelfacts;
+CREATE TABLE Movies (
+	movieID int NOT NULL,
+	title varchar(255),
+	length int,
+	director varchar(255),
+	description varchar(255),
+	PRIMARY KEY (movieID)
+);
+
+CREATE TABLE Actors (
+		actorID int NOT NULL, 
+        name varchar(255),
+        age int, 
+        birthDate date, 
+        charID int, 
+        PRIMARY KEY (actorID),
+        FOREIGN KEY (charID) REFERENCES Characters(charID)
+);
+
+CREATE TABLE Characters (
+		charID int NOT NULL, 
+        name varchar(255),
+        description varchar(255), 
+        actorID int, 
+        PRIMARY KEY (charID)
+);
+
+ALTER TABLE Characters 
+ADD FOREIGN KEY (actorID) REFERENCES Actors(actorID);
+
+CREATE TABLE inMovie (
+	movieID int, 
+    actorID int,
+    charID int, 
+    FOREIGN KEY (movieID) REFERENCES Movies(movieID),
+    FOREIGN KEY (actorID) REFERENCES Actors(actorID),
+    FOREIGN KEY (charID) REFERENCES Characters(charID)
+);
+    
+        
+
