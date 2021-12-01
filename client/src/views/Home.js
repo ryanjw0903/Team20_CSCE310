346c5698d8fc4c120df89054f06e602e2b088446
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2021-12-01 17:53:33 -0600

    Update Navbar UI and routing issues

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,23 +1,24 @@
 import React from "react";
 import './Home.css'
-import { useState } from 'react';
-import Form from "react-bootstrap/Form"
-import Button from 'react-bootstrap/Button'
+import { useState} from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+
 export default function Home(){
   const[movieName, setMovieName] = useState("");
+/*
+useEffect(() => {
+     fetch("/movies")
+       .then(res => res.json())
+      .then(res => {
+          console.log(res);
+      })
+      .catch(err => {
+        console.error("Error", err);
+    })
+  }, []);
+  */
 
-
-// useEffect(() => {
-//     fetch("/movies")
-//       .then(res => res.json())
-//       .then(res => {
-//           console.log(res);
-//           setData(res.message);
-//       })
-//       .catch(err => {
-//         console.error("Error", err);
-//       })
-//   }, []);
 function handleSubmit(event){
   console.log(movieName);
   event.preventDefault();
@@ -27,7 +28,6 @@ function handleSubmit(event){
 
 return (
     <div className="main-page">
-      <h3 className = "title">GOMARVELFACTS</h3>
     <div className="container">
       <div className="row my-5">
         <div className="col-lg-18">
