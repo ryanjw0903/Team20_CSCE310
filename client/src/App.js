106b52d525e961d29a51df1e820eafffd6e73144
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2021-12-05 15:11:45 -0600

    added back app.js file

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Home from './views/Home'
+import Login from './views/Login'
+import Admin from './views/Admin'
+import './App.css'
+import {Link, Route, Switch} from "react-router-dom";
+
+function App() {
+  return (
+      <React.Fragment>
+        <div class="nav-wrapper">
+          <h1>GoMarvelFacts</h1>
+          <nav className="nav-container">
+            <ul className="nav-style">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+
+   </React.Fragment>
+  );
+}
+
+export default App;
