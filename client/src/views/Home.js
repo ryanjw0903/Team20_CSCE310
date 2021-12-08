7c4c4b298e21fecbde55b0d488ad596ddc0808c3
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2021-12-07 19:43:21 -0600

    Created fact compment and dispalyed once title is committed

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css'
+import Facts from "./factsheet"
 import {useState, useEffect} from 'react';
 import { Form, Button } from 'react-bootstrap';
 
@@ -9,7 +10,7 @@ export default function Home(){
   const[allMovies, setAllMovies] = useState([]);
   const[allActors, setAllActors] = useState([]);
   const[allCharacters, setAllCharacters] = useState([]);
-  const[facts, setFacts] = useState([]);
+  const[facts, setFacts] = useState(null);
 
 
 // Gets SELECT * on first render of home page
@@ -17,7 +18,6 @@ useEffect(() => {
      fetch("/allMovies")
      .then(res => res.json())
       .then(res => {
-          console.log(res);
           setAllMovies(res);
       })
       .catch(err => {
@@ -27,18 +27,15 @@ useEffect(() => {
     fetch("/allActors")
        .then(res => res.json())
       .then(res => {
-          console.log(res);
           setAllActors(res);
       })
       .catch(err => {
         console.error("Error", err);
     })
 
-
     fetch("/allCharacters")
        .then(res => res.json())
       .then(res => {
-          console.log(res);
           setAllCharacters(res);
       })
       .catch(err => {
@@ -56,8 +53,7 @@ function handleSubmit(event){
   fetch("/movie", requestOptions)
   .then(res => res.json())
   .then(res => {
-     console.log(res)
-     setFacts(res)
+     setFacts(res);
   })
   .catch(err => {
    console.error("Error", err);
@@ -66,41 +62,46 @@ function handleSubmit(event){
 }
 
 return (
-    <div className="main-page">
-    <div className="container">
-      <div className="row my-5">
-        <div className="col-lg-18">
-          <div className="centered">
-              <div className="col-lg-6">
-                <Form onSubmit = {handleSubmit}>
-                  <Form.Group size = "lg">
-                  <Form.Label>Movie Name: </Form.Label>
-                  <Form.Control
-                    autoFocus
-                    type="text"
-                    value={movieName}
-                    onChange={(e) => setMovieName(e.target.value)}
-                    />
-                  </Form.Group>
-                  <Button className = "btn btn-primary" type = "submit">
-                    Enter
-                  </Button>
-                  </Form>
-              </div>
-          </div>
-
-          <div className = "row">
-              <div className = "col-lg-6" style = {{textAlign: 'center', fontSize: '25px'}}>
-                <label className = "labelInput">Fun Facts:</label>
-                <div className="col-lg-6" style = {{textAlign: 'center', backgroundColor: '#995050', color: 'white'}}>
-                  Once you submit the movie title, we'll fill this up with fun facts.<br/><br/><br/>
+    <React.Fragment>
+      <div className="container">
+        <div className="row my-5">
+          <div className="col-lg-18">
+            <div className="centered">
+                <div className="col-lg-6">
+                  <Form onSubmit = {handleSubmit}>
+                    <Form.Group size = "lg">
+                    <Form.Label>Movie Name: </Form.Label>
+                    <Form.Control
+                      autoFocus
+                      type="text"
+                      value={movieName}
+                      onChange={(e) => setMovieName(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Button className = "btn btn-primary" type = "submit">
+                      Enter
+                    </Button>
+                    </Form>
                 </div>
-              </div>
-          </div>
+            </div>
+            <div className = "row">
+                <div className = "col-lg-6" style = {{textAlign: 'center', fontSize: '25px'}}>
+                  <label className = "labelInput">Fun Facts:</label>
+                  {facts ? (
+                    <div className="col-lg-6" style = {{textAlign: 'center', backgroundColor: '#995050', color: 'white'}}>
+                      <Facts facts={facts}/>
+                    </div>
+                  ):(
+                    <div className="col-lg-6" style = {{textAlign: 'center', backgroundColor: '#995050', color: 'white'}}>
+                    Once you submit the movie title, we'll fill this up with fun facts.<br/><br/><br/>
+                    </div>
+                  )}
+                </div>
+            </div>
+        </div>
+        </div>
       </div>
-      </div>
-    </div>
-    </div>
+    </React.Fragment>
   );
   }
 
